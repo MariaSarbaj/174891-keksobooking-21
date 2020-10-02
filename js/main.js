a5644acbf962788889ade74b59ab2bd2110e21d6
MariaSarbaj <63845854+MariaSarbaj@users.noreply.github.com>
2020-10-02 20:53:32 +0300

    Активация карты, подстановка координат адреса, сопоставление гостей и комнат

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 'use strict';
+
+// Constants
+
 const ADVERTISEMENT_QUANTITY = 8;
 const TITLES = [`Большая уютная квартира`, `Маленькая неуютная квартира`, `Огромный прекрасный дворец`, `Маленький ужасный дворец`, `Красивый гостевой домик`, `Некрасивый негостеприимный домик`, `Уютное бунгало далеко от моря`, `Неуютное бунгало по колено в воде`];
 const AddressX = {
@@ -33,7 +36,19 @@ const PinSize = {
   HEIGHT: 70
 };
 
+const InitialPinPosition = {
+  X: 570,
+  Y: 375
+};
+
+const PinRoundSize = {
+  WIDTH: 65,
+  HEIGHT: 65
+};
+
 const AVATAR_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// Generate Ads
 
 const avatars = [];
 
@@ -144,5 +159,105 @@ const fillFragment = () => {
   mapPins.appendChild(fragment);
 };
 
-fillFragment();
-map.classList.remove(`map--faded`);
+// Activate Map
+
+const mapPinMain = document.querySelector(`.map__pin--main`);
+const adForm = document.querySelector(`.ad-form`);
+const adFormElements = adForm.querySelectorAll(`fieldset`);
+const filterFormElements = document.querySelectorAll(`select, fieldset`);
+
+mapPinMain.addEventListener(`mousedown`, (evt) => {
+  if (!evt.button) {
+    setActivateState();
+    fillFragment();
+  }
+});
+
+mapPinMain.addEventListener(`keydown`, (evt) => {
+  if (evt.key === `Enter`) {
+    setActivateState();
+    fillFragment();
+  }
+});
+
+const setActivateState = function () {
+  map.classList.remove(`map--faded`);
+  adForm.classList.remove(`ad-form--disabled`);
+  adFormElements.forEach((item) => {
+    item.removeAttribute(`disabled`);
+  });
+  filterFormElements.forEach((item) => {
+    item.removeAttribute(`disabled`);
+  });
+  addressInput.value = `${PinMainPosition.x}, ${PinMainPosition.y}`;
+};
+
+/* Inactivated map function
+
+const setInactivateState = function () {
+  map.classList.add(`map--faded`);
+  adForm.classList.add(`ad-form--disabled`);
+  adFormElements.forEach((item) => {
+    item.setAttribute(`disabled`, ``);
+  });
+  filterFormElements.forEach((item) => {
+    item.setAttribute(`disabled`, ``);
+  });
+  addressInput.value = `${PinMainInitialPosition.x}, ${PinMainInitialPosition.y}`
+}
+*/
+
+// Filling address input
+
+const addressInput = adForm.querySelector(`#address`);
+
+const PinMainInitialPosition = {
+  x: Math.floor(InitialPinPosition.X + PinRoundSize.WIDTH / 2),
+  y: Math.floor(InitialPinPosition.Y + PinRoundSize.HEIGHT / 2)
+};
+
+const PinMainPosition = {
+  x: Math.floor(InitialPinPosition.X + PinSize.WIDTH / 2),
+  y: Math.floor(InitialPinPosition.Y + PinSize.HEIGHT / 2)
+};
+
+if (map.classList.contains(`map--faded`)) {
+  addressInput.value = `${PinMainInitialPosition.x}, ${PinMainInitialPosition.y}`;
+}
+
+// Rooms and Guests Compare
+
+const roomLimits = {
+  1: [`1`],
+  2: [`1`, `2`],
+  3: [`1`, `2`, `3`],
+  100: [`0`]
+};
+
+let roomOptions;
+const roomNumber = adForm.querySelector(`#room_number`);
+const guestQuantity = adForm.querySelector(`#capacity`);
+const guestQuantityOptions = guestQuantity.querySelectorAll(`#capacity option`);
+
+const roomNumberChangeHandler = () => {
+  roomOptions = roomLimits[roomNumber.value];
+  guestQuantityOptions.forEach((elem) => {
+    if (roomOptions.indexOf(elem.value) === -1) {
+      elem.setAttribute(`hidden`, ``);
+    } else {
+      elem.removeAttribute(`hidden`);
+    }
+  });
+  guestNumberChangeHandler();
+};
+
+const guestNumberChangeHandler = () => {
+  roomOptions = roomLimits[roomNumber.value];
+  if (roomOptions.indexOf(guestQuantity.value) === -1) {
+    guestQuantity.setCustomValidity(`Число гостей не соответствует ограничениям для данного числа комнат!`);
+  } else {
+    guestQuantity.setCustomValidity(``);
+  }
+};
+
+roomNumber.addEventListener(`change`, roomNumberChangeHandler);
